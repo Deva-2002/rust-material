301caf26169bc07f536cd2b58c50a8f773ea183a
Deva <devakrishnasj@gmail.com>
2025-08-13 07:00:03 +0000

    finished functions and conditional statements

--- src/main.rs
+++ src/main.rs
@@ -179,3 +179,101 @@
 //     let element=arr[index];
 //     println!("element at index {index} is : {element}");
 // }
+
+// functions 
+// fn main() {
+//     another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//     println!("The value of x is: {x}");
+// }
+
+// fn five() -> i32 {
+//     5 //this means return 5; -- (you can just put 5 or return 5;)
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
+// if - control statements
+// fn main() {
+//     let number = 3;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+// if-else
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// Using if in a let Statement
+// fn main() {
+//     let condition = true;
+//     let number = if condition { 5 } else { 6 };
+
+//     println!("The value of number is: {number}");
+// }
+
+//while loop
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{number}!");
+
+//         number -= 1;
+//     }
+
+//     println!("LIFTOFF!!!");
+// }
+
+//looping through array;
+// fn main(){
+//     let arr=[1,2,3,4,5];
+//     let mut index=0;
+//     while index!=arr.len(){
+//         let val=arr[index];
+//         println!("{val}");
+//         index=index+1;
+//     }
+// }
+
+
+//2nd way
+// fn main(){
+//     let arr=[1,2,3,4,5];
+//     for elements in arr {
+//         println!("{elements}");
+//     }
+// }
+
+
+//printing numbers
+// fn main(){
+//     for number in 1..4{
+//         println!("{number}");
+//     }
+
+//     for number in (1..4).rev(){
+//         println!("{number}");
+//     }
+// }
